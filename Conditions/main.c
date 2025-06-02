156b4f287a2bff689953fdb0242d7658f964834c
p20062000 <haliloulah2006@gmail.com>
2025-06-02 12:42:51 +0000

    basics

---
+++ Conditions/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void guessNumber(int guess) {
+    // TODO: write your code here
+}
+
+int main() {
+    guessNumber(500);
+    guessNumber(600);
+    guessNumber(555);
+}
---
+++ Static/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+   int sum (int num) {
+       /**
+       * find sum to n numbers
+       */
+   }
+
+   int main() {
+       printf("%d ",sum(55));
+       printf("%d ",sum(45));
+       printf("%d ",sum(50));
+       return 0;
+   }

d4bcff1093f20c880448b6dab9186aa0bd4882e9
p20062000 <haliloulah2006@gmail.com>
2025-06-02 13:56:59 +0000

    exo array done

--- Arrays/main.c
+++ Arrays/main.c
@@ -2,12 +2,14 @@
 
 int main() {
   /* TODO: define the grades variable here */
-  int average;
+    int grades[3];  
+    int average;
 
   grades[0] = 80;
   /* TODO: define the missing grade
      so that the average will sum to 85. */
-  grades[2] = 90;
+   grades[1] = 85;
+   grades[2] = 90;
 
   average = (grades[0] + grades[1] + grades[2]) / 3;
   printf("The average of the 3 grades is: %d", average);
